fe4c1d98bbcd7a877bce17d164dad4a6e06dd935
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2022-12-11 14:06:24 +0700

    [Fajri] fixing pages

--- src/Pages/Beasiswa/Beasiswa.js
+++ src/Pages/Beasiswa/Beasiswa.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import '../../Components/Nav/Nav.css'
 
 const Beasiswa = () => {
     return (
-        <h1>Beasiswa Page</h1>
+        <>
+            <div className='w-100 align-items-start' style={{height: '70px', backgroundColor: '#FFEBAD', left: '0', top: '0'}}>
+                <div className='row w-100'>
+                    <div className='col-1'>
+                        <p>Logo</p>
+                    </div>
+                    <div className='col-10 d-flex justify-content-center'>
+                        <ul>
+                            <Link to='/' className='nav-click'>
+                                <li>Home</li>
+                            </Link>
+                            <Link to='/lomba' className='nav-click'>
+                                <li>Lomba</li>
+                            </Link>
+                            <Link to='/beasiswa' className='nav-click'>
+                                <li className='nav-active'>Beasiswa</li>
+                            </Link>
+                            <Link to='/seminar' className='nav-click'>
+                                <li>Seminar</li>
+                            </Link>
+                            <Link to='/oprec' className='nav-click'>
+                                <li>Open Recruitment</li>
+                            </Link>
+                            <Link to='/karya' className='nav-click'>
+                                <li>Karya</li>
+                            </Link>
+                        </ul>
+                    </div>
+                    <div className='col-1'>
+                        <Link to='/login' style={{textDecoration: 'none'}}>
+                            <p className='nav-login'>Login</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            
+            <h1>Beasiswa Page</h1>
+        </>
     )
 }
 
--- src/Pages/Karya/Karya.js
+++ src/Pages/Karya/Karya.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import '../../Components/Nav/Nav.css'
+
 
 const Karya = () => {
     return (
         <>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
-            <h1 className="d-flex align-items-end">Karya Page</h1>
+            <div className='w-100 align-items-start' style={{height: '70px', backgroundColor: '#FFEBAD', left: '0', top: '0'}}>
+                <div className='row w-100'>
+                    <div className='col-1'>
+                        <p>Logo</p>
+                    </div>
+                    <div className='col-10 d-flex justify-content-center'>
+                        <ul>
+                            <Link to='/' className='nav-click'>
+                                <li>Home</li>
+                            </Link>
+                            <Link to='/lomba' className='nav-click'>
+                                <li>Lomba</li>
+                            </Link>
+                            <Link to='/beasiswa' className='nav-click'>
+                                <li >Beasiswa</li>
+                            </Link>
+                            <Link to='/seminar' className='nav-click'>
+                                <li>Seminar</li>
+                            </Link>
+                            <Link to='/oprec' className='nav-click'>
+                                <li >Open Recruitment</li>
+                            </Link>
+                            <Link to='/karya' className='nav-click'>
+                                <li className='nav-active'>Karya</li>
+                            </Link>
+                        </ul>
+                    </div>
+                    <div className='col-1'>
+                        <Link to='/login' style={{textDecoration: 'none'}}>
+                            <p className='nav-login'>Login</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            
+            <h1>Karya Page</h1>
         </>
     )
 }
